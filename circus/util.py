235277533b09d3b9f0a03c328421788363066ec5
Sebastiaan Huber <mail@sphuber.net>
2019-12-10 12:01:03 +0100

    Replace `IOLoop.instance` with `IOLoop.current`
    It is no longer necessary to call `instance` for normal use cases.
    Calling `current` will return the correct one and create one if it has
    not yet been instantiated.

--- circus/util.py
+++ circus/util.py
@@ -1040,7 +1040,7 @@ def tornado_sleep(duration):
     To use with a gen.coroutines decorated function
     Thanks to http://stackoverflow.com/a/11135204/433050
     """
-    return gen.Task(IOLoop.instance().add_timeout, time.time() + duration)
+    return gen.sleep(duration)
 
 
 class TransformableFuture(concurrent.Future):
